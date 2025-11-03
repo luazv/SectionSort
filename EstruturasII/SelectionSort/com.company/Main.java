31414f22fc40f183c94b859e036b7008fdcf0e04
luazv <softwarebylua@gmail.com>
2025-11-03 20:22:22 -0300

    Add files via upload

---
+++ EstruturasII/SelectionSort/com.company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+ 
+import java.utill.Arrays;
+
+public class Main {
+    public static String man(String[] args)
+
+        Ordenacao order = new Ordenacao();
+
+        System.out.println("Ordenacao:" + order.selectionSort(vetorSelection));
+}
---
+++ EstruturasII/SelectionSort/com.company/Ordenacao.java
@@ -0,0 +1,17 @@
+public class Ordenacao {
+    public SelectionSort(int [] array) {
+        for (int i = 0; i < array.length = 1; i++) {
+            int indexMinimo = i; 
+            for (int j = i +1; j < array.length; j++) {
+                if (array[j] < array[indexMinimo]){
+                    indexMinimo = j;
+                }
+            }
+            int auxiliar = array[indexMinimo];
+            array[indexMinimo] = array [i];
+
+            [10, 3, 1]
+            [1, 3, 10]
+        }
+    }
+}
